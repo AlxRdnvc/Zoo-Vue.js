e4e65a829013daabe4fad2ad45deb0a9e4e18ac4
Aleksandar_Radanovic <a.radanovic1984@gmail.com>
2018-08-07 21:29:55 +0200

    zadatak 6

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -3,8 +3,8 @@
         <h1>Animal List</h1>
 
         <form @submit.prevent>
-            <label for="kind">Kind</label>
-            <input v-model="newAnimal.kind" type="text" placeholder="kind"><br>
+            <label for="species">Species</label>
+            <input v-model="newAnimal.species" type="text" placeholder="species"><br>
 
             <label for="name">Name</label>
             <input v-model="newAnimal.name" type="text" placeholder="name"><br>
@@ -12,23 +12,30 @@
             <label for="birth">Birth</label>
             <input v-model="newAnimal.birth" type="text" placeholder="birth"><br>
 
+            <label for="sectors">Select Sector</label>
+            <select v-model="newAnimal.sector">
+                <option v-for="(sector, key) in sectors" :key="key" v-bind:value="sector.name">{{ sector.name }}</option>
+            </select><br>
+
             <button @click="addNewAnimal" type="submit">Add Animal</button>
         </form>
 
         <table>
             <thead>
-                <th>Kind</th>
+                <th>species</th>
                 <th>Name</th>
                 <th>Birth</th>
+                <th>Sector</th>
                 <th></th>
                 <th></th>
             </thead>
             <tbody>
                 <tr v-for="(animal, key) in animals" :key="key">
-                    <td>{{ animal.kind }}</td>
+                    <td>{{ animal.species }}</td>
                     <td>{{ animal.name }}</td>
                     <td v-if="!animal.birth">unknown</td>
                     <td v-else>{{ animal.birth}}</td>
+                    <td>{{ animal.sector }}</td>
                     <td><button @click="moveToTop(animal)">Move to top</button></td>
                     <td><button @click="deleteAnimal(animal)">Remove Animal</button></td>
                 </tr>
@@ -38,25 +45,29 @@
 </template>
 
 <script>
+
+const sectors = [
+    { name: "north"},
+    { name: "east"},
+    { name: "south"},
+    { name: "west"}
+]
+
 export default {
   name: 'AnimalList',
   data() {
       return {
-          newAnimal: {
-              kind:'',
-              name:'',
-              birth:''
-          },
-
-          animals: [
-              {kind:"monkey", name: "Srecko", birth: "10.10.2016"},
-              {kind:"dog", name: "Avram", birth: "09.10.2016"},
-              {kind:"cat", name: "Zora", birth: ""},
-              {kind:"bear", name: "Vidoje", birth: "21.12.1985"},
-              {kind:"fish", name: "Micko", birth: "11.04.2018"},
-          ]
-      };
-  },
+            newAnimal: {},
+            sectors: sectors,
+            animals: [
+              {species:"monkey", name: "Srecko", birth: "10.10.2016"},
+              {species:"dog", name: "Avram", birth: "09.10.2016"},
+              {species:"cat", name: "Zora", birth: ""},
+              {species:"bear", name: "Vidoje", birth: "21.12.1985"},
+              {species:"fish", name: "Micko", birth: "11.04.2018"},
+            ]
+         };
+    },
   methods: {
       deleteAnimal(animal){
           console.log(animal);
