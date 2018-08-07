3b2e251ee8a9e2bc1b1aa4f9762c5b61c5a1b43c
Aleksandar_Radanovic <a.radanovic1984@gmail.com>
2018-08-07 18:30:53 +0200

    zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AnimalList />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AnimalList from './components/AnimalList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    AnimalList
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/AnimalList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Animal List</h1>
+        <table>
+            <thead>
+                <th>Kind</th>
+                <th>Name</th>
+                <th>Birth</th>
+            </thead>
+            <tbody>
+                <tr v-for="(animal, key) in animals" :key="key">
+                    <td>{{ animal.kind }}</td>
+                    <td>{{ animal.name }}</td>
+                    <td>{{ animal.birth }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+  data() {
+      return {
+          animals: [
+              {kind:"monkey", name: "Srecko", birth: "10.10.2016"},
+              {kind:"dog", name: "Avram", birth: "09.10.2016"},
+              {kind:"cat", name: "Zora", birth: "08.05.2017"},
+              {kind:"bear", name: "Vidoje", birth: "21.12.1985"},
+              {kind:"fish", name: "Micko", birth: "11.04.2018"},
+          ]
+      };
+  }
+}
+</script>
+
+
